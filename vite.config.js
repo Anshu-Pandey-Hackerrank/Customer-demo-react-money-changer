4f14ab542e1d33a055bf7c0617f6e7960c2b4dc6
Anshu-Pandey-Hackerrank <anshu.pandey@problemsetters.com>
2025-01-09 16:57:22 +0530

    preview port expose

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+    strictPort: true,
+    cors: true,
+    hmr: {
+      clientPort: 3000
+    }
+  }
 })
